b4e99d58530d91255b4c0511dd25103a3034d053
Anton K <antonk@MacBook-Air-Anton.local>
2022-04-18 12:51:20 +0300

    DZ

---
+++ src/components/users.jsx
@@ -0,0 +1,77 @@
+import React, {useState} from "react"
+import api from "../api"
+
+const Users = () => {
+    const [users, setUser] = useState(api.users.fetchAll())
+    // console.log(api.users.fetchAll());
+    const renderPhrase = (number) => {
+        const lastOne = Number(number.toString().slice(-1))
+        if (number > 4 && number < 15) return 'человек тусанет'
+        if ([2, 3, 4].indexOf(lastOne) >= 0) return 'человека таусанут'
+        if (lastOne === 1) return 'человек тусанет'
+        return 'человек тусанет'
+    };
+    const handleDeleteUser = (id) => {
+        setUser((prevState) => prevState.filter((user) => user._id !== id))
+    };
+    
+    
+    return (
+        <>
+            <h2>
+                <span
+                    className={'badge' + (users.length > 0 ? 'bg-primary' : 'bg-danger')}
+                >
+                    {users.length > 0
+                        ? `${users.length + ' ' + renderPhrase(users.length)} с тобой`
+                        : 'Никто с тобой не тусанет'}
+                </span>
+            </h2>
+            {users.length > 0 && (
+            <table className="table">
+                <thead>
+                    <tr>
+                    <th scope="col">Имя</th>
+                    <th scope="col">Качество</th>
+                    <th scope="col">Профессия</th>
+                    <th scope="col">Встретились</th>
+                    <th scope="col">Оценка</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user) => (
+                    <tr key={user._id}>
+                        <td>{user.name}</td>
+                        <td>
+                            {user.qualities.map((item) => (
+                                <span
+                                    className={'badge m-1 bg-' + item.color}
+                                    key={item._id}
+                                >
+                                    {item.name}
+                                </span>
+                            ))}
+                        </td>
+                        <td>{user.profession.name}</td>
+                        <td>{user.completedMeetings}</td>
+                        <td>{user.rate}</td>
+                        <td>
+                            <button
+                                className='btn btn-danger'
+                                onClick={() => handleDeleteUser(user._id)}
+                            >
+                                delete
+                            </button>
+                        </td>
+                    </tr>
+                    ))}
+                    
+                </tbody>
+            </table>
+            )}
+        </>
+    );
+    // return <h1>Users</h1>;
+};
+
+export default Users;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import Users from "./components/users";
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Users />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
